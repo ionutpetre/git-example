09cd73cc02a371b15789d620bb48ec342941d549
Ionuţ Petre <ionut.petre@ro.ibm.com>
2016-03-27 13:31:19 +0300

    Added some code...

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+var helloMessage = 'Hello World, from ',
+    helloEmoticon = ' :)',
+    promptMessage = 'Please enter your name',
+    //Change this line of code, by adding your name
+    defaultName = 'Ionut Petre';
+
+function setHeading1(name) {
+    var h1Elem = document.getElementsByTagName('h1')[0];
+    h1Elem.innerHTML = helloMessage + name + helloEmoticon;
+}
+
+function bodyLoadHandler() {
+    setHeading1(defaultName);
+}
+
+function btnClickHandler() {
+    yourName = prompt(promptMessage, defaultName);
+    if (yourName != null) {
+        setHeading1(yourName);
+    }
+}
